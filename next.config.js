53d1c8c3778ebca7ad1b2ab6c084790d0167ae24
Jongchan Kim <8eatles@naver.com>
2021-06-30 21:09:33 +0900

    api server proxy config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_SERVER_PATH}/api/:path*`,
+      },
+    ];
+  },
+};
